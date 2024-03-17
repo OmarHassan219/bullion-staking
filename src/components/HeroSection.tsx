71ae2691db7a3a095a9b0aa4632462e4acdc81ef
OmarHassan219 <ramo-3000@live.com>
2024-03-17 15:59:56 +0400

    lang edit

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -19,15 +19,15 @@ const t = useTranslations('Hero');
       <Flexbox direction="col" justify="center" className=" text-white gap-[30px] p-4 xl:ml-[137px] h-full">
         <Flexbox direction="col" className="  gap-[13px]">
           <h1 className="capitalize text-3xl md:text-4xl lg:leading-[1] lg:text-[46px] font-light text-white ">
-            join the future <br /> Of{" "}
-            <span className="font-semibold">financial empowerment</span>
+            {t('join the future')} <br /> {t('of')}
+            <span className="font-semibold">{t('header')}</span>
           </h1>
           <p className="text-lg ">
-            Choose a place where your savings transform into gold,
-            <br /> blending modern finance with timeless value.
+            {t('Gold')}
+            <br /> {t('desc')}
           </p>
         </Flexbox>
-        <button className="font-medium text-xs w-fit sm:text-sm md:text-xl sm:px-5 py-[7px]  bg-primary text-white px-3 rounded-full border border-transparent focus:shadow-none duration-300 hover:border-primary hover:bg-transparent hover:text-primary " type="button">{t('about')}</button>
+        <button className="font-medium text-xs w-fit sm:text-sm md:text-xl sm:px-5 py-[7px]  bg-primary text-white px-3 rounded-full border border-transparent focus:shadow-none duration-300 hover:border-primary hover:bg-transparent hover:text-primary " type="button">{t('know more')}</button>
       </Flexbox>
     </div>
   );
